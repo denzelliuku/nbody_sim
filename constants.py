110491cc7bb825281ae82d1af2161c423d50f26d
henri <henri.rapeli@gmail.com>
2023-02-14 14:12:00 +0200

    Created a new file to store some useful constants

---
+++ constants.py
@@ -0,0 +1,8 @@
+"""
+Some useful constants/conversion factors used in the simulations
+"""
+
+big_g = 6.67259e-11  # Gravitational constant [N*m^2/kg^2]
+m_e = 5.97217e24  # Mass of the Earth [kg]
+au = 1.495978707e11  # Astronomical unit [m]
+r_e = 6.371e6  # Mean radius of earth [m]
